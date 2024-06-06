610baf8c486fcc2f1d9954358e64418144e17c7c
The Bored CS Student <93075473+theboredcsstudent@users.noreply.github.com>
2024-06-07 06:46:07 +0800

    Delete Contains Duplicate.py

--- Contains Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
